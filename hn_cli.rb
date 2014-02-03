f591051ad43018f7db138ab760aa1f3bd9705d3c
Gary Patricelli <garypatricelli@gmail.com>
2014-02-03 15:32:56 -0500

    fix sorting

--- hn_cli.rb
+++ hn_cli.rb
@@ -20,42 +20,53 @@ end
 doc = Nokogiri::HTML(open("https://news.ycombinator.com/"))
 
 # Pull data fields from the ".title" CSS tag 
-name_count = 0 # The number of names that have been put into the stories array
-source_count = 0 # The number of sources that have been put into the stories array
+story_count = 0 # The story number we are currently processing 
 doc.css('.title~ .title').each do |title|
     if title.at_css('a').class == Nokogiri::XML::Element
         # This is the story title, store it in the array and increment the count
-        stories[name_count].name = title.at_css('a').text # Story name
-        name_count += 1
+        stories[story_count].name = title.at_css('a').text # Story name
     end
     if title.at_css('.comhead').class == Nokogiri::XML::Element
         # This is the story source, store it in the array and increment the count
-        stories[source_count].source = title.at_css('.comhead').text # Story source
-        source_count += 1
+        stories[story_count].source = title.at_css('.comhead').text # Story source
+    else
+        # This is a blank field, it may be a post from ycombinator
+        # Add a filler so that sorting is not broken
+        stories[story_count].source = '(ycombinator)'
     end
+    story_count += 1
 end
 
 # Pull data fields from the ".subtext" CSS tag
-point_count = 0 # The number of points that have been put into the stories array
-submitter_count = 0 # The number of submitters that have been put into the stories array
-comment_count = 0 # The number of comments that have been put into the stories array
+story_count = 0 # The story number we are currently processing 
 doc.css('.subtext').each do |subtext|
     if subtext.at_css('span').class == Nokogiri::XML::Element
         # This is the number of points the story receive, store it in the array and increment the count
-        stories[point_count].points = subtext.at_css('span').text # Story points
-        point_count += 1
+        stories[story_count].points = subtext.at_css('span').text[0..-8].to_i # Story points
+    else
+        # This is a blank field, it may be a post from ycombinator
+        # Add a filler so that sorting is not broken
+        stories[story_count].points = 0
     end
     if subtext.at_css('span+ a').class == Nokogiri::XML::Element
         # This is the submitter's name, store it and increment the count
-        stories[submitter_count].submitter = subtext.at_css('span+ a').text # The user that submitted the story 
-        submitter_count += 1
+        stories[story_count].submitter = subtext.at_css('span+ a').text # The user that submitted the story 
+    else
+        # This is a blank field, it may be a post from ycombinator
+        # Add a filler so that sorting is not broken
+        stories[story_count].submitter = 'ycombinator'
     end
     if subtext.at_css('a+ a').class == Nokogiri::XML::Element
         # This is the number of comments for the story, store it and increment the count
-        stories[comment_count].comments =  subtext.at_css('a+ a').text # Story comment count
-        comment_count += 1
+        stories[story_count].comments =  subtext.at_css('a+ a').text[0..-8].to_i # Story comment count
+    else
+        # This is a blank field, it may be a post from ycombinator
+        # Add a filler so that sorting is not broken
+        stories[story_count].comments = 0
     end
+    story_count += 1
 end
+
 
 # Check if the user requested sorting
 if ARGV[0] == '-s'
@@ -84,7 +95,7 @@ end
 stories.each do |story| 
    puts
    puts "   #{story.name}   #{story.source}"
-   puts "       < #{story.points} | #{story.submitter} | #{story.comments} > "
+   puts "       < #{story.points} points | #{story.submitter} | #{story.comments} comments > "
 end
 
 # Give credit where credit is due...
